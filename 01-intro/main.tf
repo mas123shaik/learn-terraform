710aee1090de88f52087372e6f04e1ace00b18d4
mas123shaik <masthan.shaik@gmail.com>
2025-02-26 11:02:45 +0530

    terraform changes

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -5,3 +5,62 @@ resource "local_file" "foo" {
   filename = "/tmp/foo.bar"
 }
 
+# Normal Variable
+variable "x" {
+  default = 10
+}
+
+# List variable
+
+variable "y" {
+  default = [ 1, "abc", 0.3, false]
+}
+# list can have a combination of types also
+
+variable "z" {
+  default = {
+    name = "JOhn"
+    age = 20
+  }
+}
+
+## Print variable on output
+output "x" {
+  value = var.x
+}
+
+# if our value is just variable then var.x , if it is combination of strings then "${var.x}"
+output "x1" {
+  value = "Value of x - ${var.x}"
+}
+
+output "y1" {
+  value = var.y[0]
+}
+
+output "z" {
+  value = "Name - ${var.z["name"]}, Age - ${var.z.age}"
+}
+
+#### Variables from another files
+
+# tfvars - manually loaded, Meaning these files are loaded to command line by using -var-file option
+# terraform apply -auto-approve -var-file=data.tfvars
+
+variable "input" {}
+output "input" {
+  value = var.input
+}
+
+
+# auto.tfvars - Automatically these files are loaded
+variable "class" {}
+output "class" {
+  value = var.class
+}
+
+variable "trainer" {}
+output "trainer" {
+  value = var.trainer
+}
+
